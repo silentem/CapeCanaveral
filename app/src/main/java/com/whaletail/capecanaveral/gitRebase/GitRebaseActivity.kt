7c4e30aef0e346ada358baa3079c7be9ff68a0d7
artemkovalenko <silentem1113@gmail.com>
2019-07-06 15:14:18 +0300

    add git rebase activity

---
+++ app/src/main/java/com/whaletail/capecanaveral/gitRebase/GitRebaseActivity.kt
@@ -0,0 +1,13 @@
+package com.whaletail.capecanaveral.gitRebase
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.whaletail.capecanaveral.R
+
+class GitRebaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_rebase)
+    }
+}
